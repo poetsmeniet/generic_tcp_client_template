69ea4c6d9c506954d24ab34849a32fca72f6f15b
Thomas <thomas@geenbs.nl>
2017-10-19 22:30:56 +0200

    Added example code for usage

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include "tcp_client.h"
 //This file demonstrates usage of tcp_client library
 
@@ -9,30 +11,34 @@ int main(void){
     int serverPort =  80;
     int clientSocket;
 
-    clientSocket = connectToServer(serverName, serverPort);
+    clientSocket = connectToServer(serverName, serverPort, 10);
     if(clientSocket == 0){
         printf("Connect failed with code %d\n", clientSocket);
         return 1;
     }
-
-    //Send a message and get response(s)
+    
+    //Send a message
     int rc;
-    int respCnt = 1;
-    respBuf *responses = (respBuf*) malloc(respCnt * sizeof(respBuf));
-    rc = sendMessage(&clientSocket, "GET /test.html HTTP/1.1\nhost: digiploert.nl\nConnection: Keep-Alive\n\n", 68,\
-            respCnt, responses);
+    char *req = malloc(2000 * sizeof(char));
+    memcpy(req, "GET /index.php HTTP/1.0\n\n", 25);
+
+    rc = sendMessage(&clientSocket, req, strlen(req));
     if(rc == 0){
-        printf("Sending messsage failed.\n");
+        printf("Sending messsage 1 failed.\n");
         return 1;
     }
-    //Print response(s)
-    int i;
-    for(i = 0; i < respCnt; i++){
-        printf("\n*********** response Nr %d:\n'%s'\n\n",responses[i].nr , responses[i].buffer);
-    }
-    
-    //free(responses);
+    memset(req,0,strlen(req));
 
+    //Get response
+    size_t respCnt = 3; //get some chunks
+    respBuf *responses = (respBuf*) malloc(respCnt * sizeof(respBuf));
+    recvMessage(&clientSocket, responses, respCnt);
+    size_t i;
+    for(i = 0; i < respCnt; i++)
+        printf("%s", responses[i].buffer);
+    
+    free(responses);
+    free(req);
     //closeSocket();
 
     return 0;
